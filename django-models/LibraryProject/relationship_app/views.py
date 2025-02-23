832f92dbf8547d76c269b62c38a8239694be39b9
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-02-23 20:33:18 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic import DetailView
 from django.shortcuts import render
 from .models import Book  # Import the Book model
