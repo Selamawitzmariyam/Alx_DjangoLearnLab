8fc715990260b0501fcf2df00e309fd0e4a1d5e2
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-03-17 13:57:27 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from rest_framework.permissions import IsAuthenticated, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
