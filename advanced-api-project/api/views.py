c396ac8d065e7a42786d61e555cb618ab807e9c6
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-03-17 13:49:40 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
