0c2d2a7d18c1a29436006e6081b87202b61a1e38
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-09 20:59:01 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in auth routes
 
-    path('signup/', SignUpView.as_view(), name='signup'),    # Custom registration
+    path('signup/', views.register,name='register'),    # Custom registration
 
 ]
