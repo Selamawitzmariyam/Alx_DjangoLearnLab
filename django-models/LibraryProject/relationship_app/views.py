ce41c537725b9c4a3e8f18ae9df75c43a637173c
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-09 21:10:29 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from django.contrib.auth import login,logout
 class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')  # Redirect to login after signup
