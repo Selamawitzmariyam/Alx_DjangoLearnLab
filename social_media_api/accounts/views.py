8dc50258af5c1e3aab6d5d43702feb19f2529dc9
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-10 16:59:57 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer
