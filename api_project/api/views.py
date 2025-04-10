1e9906337afb4859b45107464dcd5dbebd26c718
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-10 11:29:32 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,14 @@
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer  # Make sure you have a serializer for the Book model
+
+class BookViewSet(viewsets.ModelViewSet):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 class BookList(generics.ListAPIView):
         queryset = Book.objects.all
         serializer_class = BookSerializer
