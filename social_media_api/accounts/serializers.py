97d9f35971f8e1e71e4c8ac9d0e4d0b55b6ccb73
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-10 14:30:36 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,34 +1,22 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
-        
+from rest_framework.exceptions import ValidationError
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture')
 
     def validate(self, attrs):
-        """
-        Check that the two passwords match.
-        """
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords must match"})
+            raise ValidationError({"password": "Passwords must match"})
         return attrs
 
     def create(self, validated_data):
-        """
-        Create and return a user with encrypted password and additional data.
-        """
         validated_data.pop('password2', None)  # Remove password2
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
@@ -37,4 +25,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        return user
+
+        # Generate and return token for the user
+        token = Token.objects.create(user=user)
+        return user, token
