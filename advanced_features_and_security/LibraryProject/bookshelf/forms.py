cc15317cbaa7b19dacb08b1f8b4ef0b67b9073d6
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-04-10 10:47:59 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(label='Search Books', max_length=100)
+class ExampleForm(forms.Form):
+    # Define your form fields
+    example_field = forms.CharField(max_length=100, label='Example Field')
